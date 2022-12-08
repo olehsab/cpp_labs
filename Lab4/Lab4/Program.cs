aa6a8951222bc362790124e150f9c6a7e5340132
olehsab <119971190+olehsab@users.noreply.github.com>
2022-12-08 23:59:13 +0200

    Init

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,52 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+var app = new CommandLineApplication();
+
+var path = "";
+
+app.Command("run", runCmd =>
+{
+    runCmd.OnExecute(() =>
+    {
+        Console.WriteLine("Specify a subcommand");
+        runCmd.ShowHelp();
+    });
+    runCmd.Command("lab3", labExecute =>
+    {
+        runCmd.Command("setPath", pathExecute =>
+        {
+            var option = pathExecute.Option("-p||--path", "Specify path to lab", CommandOptionType.SingleValue).IsRequired();
+            pathExecute.OnExecute(() =>
+            {
+                if (option.Values.Any()) path = option.Values[0];
+            });
+        });
+        
+        labExecute.OnExecute(() =>
+        {
+            var input = labExecute.Option("-i|--input", "Input file name", CommandOptionType.SingleValue);
+            var output = labExecute.Option("-o|--output", "Output file name", CommandOptionType.SingleValue);
+            if (path is not "")
+            {
+                var lab = new LabExecution.LabExecution();
+                lab.Run(path);
+            }
+            else
+            {
+                Console.WriteLine("There this no path to execute lab!");
+            }
+        });
+    });
+});
+
+app.Command("version", versionCmd =>
+{
+    versionCmd.OnExecute(() =>
+    {
+        Console.WriteLine("Oleg Sabadaha");
+        Console.WriteLine("Version 1.0");
+    });
+});
+
+
+return app.Execute(args);
---
+++ Lab4/LabExecution/LabExecution.cs
@@ -0,0 +1,9 @@
+﻿namespace LabExecution;
+
+public class LabExecution
+{
+    public void Run(string path)
+    {
+        AppDomain.CurrentDomain.ExecuteAssembly(path);
+    }
+}
